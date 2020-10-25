ec2a6609af403c7d26920d88fb1e437242907868
chandu283 <chandu.cv06@gmail.com>
2020-10-25 16:45:22 -0400

    first commit

---
+++ src/test/java/TestProject/AppiumTest.java
@@ -0,0 +1,18 @@
+package TestProject;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAndroid()
+	{
+	System.out.println(" NativeAppAndroid");
+
+}
+	@Test
+	public void Ios()
+	{
+	System.out.println("IosApps");
+
+}
+}
